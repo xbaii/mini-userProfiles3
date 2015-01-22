d288e72aab0fb2646730cbce6ad068d22a27642a
Bailey Leising <bleising10@gmail.com>
2015-01-22 12:15:58 -0700

    finished

--- service.js
+++ service.js
@@ -1,12 +1,32 @@
 var app = angular.module('userProfiles');
 
 app.service('mainService', function($http, $q) {
+//   this.getUsers = function() {
+//     var deferred = $q.defer();
+//     $http({
+//         method: 'GET',
+//         url: 'http://reqr.es/api/users?page=1'
+//     }).then(function(response){
+//       response = response.data.data
+//       deferred.resolve(response)
+//     })
+//     return deferred.promise;
+//   }
+// });
 
 
-  this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqr.es/api/users?page=1'
-    })
-  }
-});
+    this.getUsers = function() {
+      var deferred = $q.defer();
+      $http({
+          method: 'GET',
+          url: 'http://reqr.es/api/users?page=1'
+      }).then(function(response){
+        response = response.data.data
+        for(var i = 0; i < response.length; i++){
+          response[i].first_name = 'Ralf';
+        }
+        deferred.resolve(response)
+      })
+      return deferred.promise;
+    }
+  });
